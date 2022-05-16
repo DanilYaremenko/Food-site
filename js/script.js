ad2e0bb3703fe403e25bb2771e913fe2c119fdad
Veritate <danilyaremenko@gmail.com>
2022-05-16 05:24:06 +0300

    adding countdown timer

--- js/script.js
+++ js/script.js
@@ -39,4 +39,51 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
     });
 
+    //Timer
+
+    const deadline = '2022-06-11';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor( (t / (1000 * 60 * 60 * 24)) ),
+            seconds = Math.floor( (t / 1000) % 60 ),
+            minutes = Math.floor( (t / 1000 / 60) % 60 ),
+            hours = Math.floor( (t / (1000 * 60 * 60) % 24) );
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(selector, endtime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
 });
