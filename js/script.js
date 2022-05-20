f4a7639d77a66c826450b7d6c9f0e44efbfee26e
Veritate <danilyaremenko@gmail.com>
2022-05-20 18:55:01 +0300

    closing modal window when clicking Escape button

--- js/script.js
+++ js/script.js
@@ -108,17 +108,24 @@ window.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    modalCloseBtn.addEventListener('click', () => {
+    function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show');
         document.body.style.overflow = '';
-    });
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
 
     modal.addEventListener('click', (e) => {
         if (e.target === modal) {
-            modal.classList.add('hide');
-            modal.classList.remove('show');
-            document.body.style.overflow = '';
+            closeModal();
+        }
+    });
+    
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) { 
+            closeModal();
         }
     });
 
