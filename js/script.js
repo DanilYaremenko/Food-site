ed0a65535e970687e64881796610a7911a01b685
Veritate <danilyaremenko@gmail.com>
2022-05-27 03:43:09 +0300

    creating a server which can work with POST requests. setRequestHeader is not needed for XMLHttpRequest + FormData. When I used it response was array(0). Fixing 404 (Not Found) error failed to load favicon.ico.

--- js/script.js
+++ js/script.js
@@ -210,4 +210,49 @@ window.addEventListener('DOMContentLoaded', function() {
         21,
         ".menu .container"
     ).render();
+
+    //Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            const formData = new FormData(form);
+
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
